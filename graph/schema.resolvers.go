baeeba87d0190558d3f3a50400b6af4811554b1a
Laura <laurakcleve@gmail.com>
2022-05-31 10:23:02 -0600

    Change error messages, add item resolvers

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -67,107 +67,107 @@ func (r *queryResolver) ItemByID(ctx context.Context, id string) (*model.Item, e
 }
 
 func (r *queryResolver) ItemByName(ctx context.Context, name string) (*model.Item, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("ItemByName not implemented"))
 }
 
 func (r *queryResolver) Dishes(ctx context.Context) ([]*model.Dish, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("Dishes not implemented"))
 }
 
 func (r *queryResolver) Dish(ctx context.Context, id string) (*model.Dish, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("Dish not implemented"))
 }
 
 func (r *queryResolver) InventoryItems(ctx context.Context) ([]*model.InventoryItem, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("InventoryItems not implemented"))
 }
 
 func (r *queryResolver) InventoryItem(ctx context.Context, id string) (*model.InventoryItem, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("InventoryItem not implemented"))
 }
 
 func (r *queryResolver) ItemLocations(ctx context.Context) ([]*model.ItemLocation, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("ItemLocations not implemented"))
 }
 
 func (r *queryResolver) ItemCategories(ctx context.Context) ([]*model.ItemCategory, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("ItemCategories not implemented"))
 }
 
 func (r *queryResolver) DishTags(ctx context.Context) ([]*model.DishTag, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("DishTags not implemented"))
 }
 
 func (r *queryResolver) Purchases(ctx context.Context) ([]*model.Purchase, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("Purchases not implemented"))
 }
 
 func (r *queryResolver) Purchase(ctx context.Context, id string) (*model.Purchase, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("Purchase not implemented"))
 }
 
 func (r *queryResolver) PurchaseLocations(ctx context.Context) ([]*model.PurchaseLocation, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("PurchaseLocations not implemented"))
 }
 
 func (r *mutationResolver) DeleteItem(ctx context.Context, id string) (*int, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("DeleteItem not implemented"))
 }
 
 func (r *mutationResolver) AddPurchase(ctx context.Context, date string, location string) (*model.Purchase, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("AddPurchase not implemented"))
 }
 
 func (r *mutationResolver) DeletePurchase(ctx context.Context, id string) (*int, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("DeletePurchase not implemented"))
 }
 
 func (r *mutationResolver) AddPurchaseItem(ctx context.Context, purchaseID string, name string, price *float64, weightAmount *float64, weightUnit *string, quantityAmount *float64, quantityUnit *string, number int, itemType string) (*model.PurchaseItem, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("AddPurchaseItem not implemented"))
 }
 
 func (r *mutationResolver) UpdatePurchaseItem(ctx context.Context, id string, name string, price *float64, weightAmount *float64, weightUnit *string, quantityAmount *float64, quantityUnit *string) (*model.PurchaseItem, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("UpdatePurchaseItem not implemented"))
 }
 
 func (r *mutationResolver) DeletePurchaseItem(ctx context.Context, id string) (*string, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("DeletePurchaseItem not implemented"))
 }
 
 func (r *mutationResolver) AddInventoryItem(ctx context.Context, name string, addDate *string, expiration *string, amount *string, defaultShelflife *string, category *string, location *string, itemType string, number int) (*model.InventoryItem, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("AddInventoryItem not implemented"))
 }
 
 func (r *mutationResolver) UpdateInventoryItem(ctx context.Context, id string, addDate *string, expiration *string, amount *string, location *string, category *string, itemType *string) (*model.InventoryItem, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("UpdateInventoryItem not implemented"))
 }
 
 func (r *mutationResolver) DeleteInventoryItem(ctx context.Context, id string) (*string, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("DeleteInventoryItem not implemented"))
 }
 
 func (r *mutationResolver) EditItem(ctx context.Context, id string, name string, categoryID *int, defaultLocationID *int, defaultShelflife *int, itemType string, countsAs []*string) (*model.Item, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("EditItem not implemented"))
 }
 
 func (r *mutationResolver) AddDish(ctx context.Context, name string, tags []*string, isActive bool, ingredientSets []*model.IngredientSetInput) (*model.Dish, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("AddDish not implemented"))
 }
 
 func (r *mutationResolver) UpdateDish(ctx context.Context, id string, name string, tags []*string, isActive bool, ingredientSets []*model.IngredientSetInput) (*model.Dish, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("UpdateDish not implemented"))
 }
 
 func (r *mutationResolver) DeleteDish(ctx context.Context, id string) (string, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("DeleteDish not implemented"))
 }
 
 func (r *mutationResolver) AddDishDate(ctx context.Context, dishID string, date string) (*model.DishDate, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("AddDishDate not implemented"))
 }
 
 func (r *mutationResolver) DeleteDishDate(ctx context.Context, id string) (*string, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("DeleteDishDate not implemented"))
 }
 
 func (r *itemResolver) Category(ctx context.Context, obj *model.Item) (*model.ItemCategory, error) {
@@ -191,71 +191,135 @@ func (r *itemResolver) Category(ctx context.Context, obj *model.Item) (*model.It
 }
 
 func (r *itemResolver) Dishes(ctx context.Context, obj *model.Item) ([]*model.Dish, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("Item Dishes not implemented"))
 }
 
 func (r *itemResolver) DefaultLocation(ctx context.Context, obj *model.Item) (*model.ItemLocation, error) {
-	panic(fmt.Errorf("not implemented"))
+	var location model.ItemLocation
+	var id int
+	err := db.Conn.QueryRow(context.Background(), `
+      SELECT inventory_item_location.* 
+      FROM inventory_item_location
+      INNER JOIN item on item.default_location_id = inventory_item_location.id
+      WHERE item.id = $1
+		`, obj.ID).Scan(&id, &location.Name)
+
+	location.ID = strconv.Itoa(id)
+	
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	return &location, nil
 }
 
 func (r *itemResolver) Purchases(ctx context.Context, obj *model.Item) ([]*model.PurchaseItem, error) {
-	panic(fmt.Errorf("not implemented"))
+
+	fmt.Println("Getting purchases")
+	
+	rows, err := db.Conn.Query(context.Background(), `
+      SELECT  
+				id,
+				price,
+        weight_amount AS "weightAmount", 
+        weight_unit AS "weightUnit",
+        quantity_amount AS "quantityAmount",
+        quantity_unit AS "quantityUnit"
+        purchase_id AS "purchaseId"
+      FROM purchase_item
+      WHERE item_id = $1
+		`, obj.ID)
+
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var purchases []*model.PurchaseItem
+
+	for rows.Next() {
+		var purchase model.PurchaseItem
+		var purchaseID int
+
+		err := rows.Scan(
+			&purchaseID, 
+			purchase.Price, 
+			purchase.WeightAmount, 
+			purchase.WeightUnit,
+			purchase.QuantityAmount,
+			purchase.QuantityUnit,
+			purchase.PurchaseID,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		purchase.ID = strconv.Itoa(purchaseID)
+		purchases = append(purchases, &purchase)
+	}
+
+	if rows.Err() != nil {
+		return nil, err
+	}
+
+	return purchases, nil
 }
 
 func (r *itemResolver) CountsAs(ctx context.Context, obj *model.Item) ([]*model.Item, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("Item CountsAs not implemented"))
 }
 
 func (r *dishResolver) Tags(ctx context.Context, obj *model.Dish) ([]*model.DishTag, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("Dish Tags not implemented"))
 }
 
 func (r *dishResolver) Dates(ctx context.Context, obj *model.Dish) ([]*model.DishDate, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("Dish Dates not implemented"))
 }
 
 func (r *dishResolver) IngredientSets(ctx context.Context, obj *model.Dish) ([]*model.IngredientSet, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("Dish IngredientSets not implemented"))
 }
 
 func (r *ingredientResolver) Item(ctx context.Context, obj *model.Ingredient) (*model.Item, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("Ingredient Item not implemented"))
 }
 
 func (r *ingredientSetResolver) Ingredients(ctx context.Context, obj *model.IngredientSet) ([]*model.Ingredient, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("IngredientSet Ingredients not implemented"))
 }
 
 func (r *inventoryItemResolver) Item(ctx context.Context, obj *model.InventoryItem) (*model.Item, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("InventoryItem Item not implemented"))
 }
 
 func (r *inventoryItemResolver) Location(ctx context.Context, obj *model.InventoryItem) (*model.ItemLocation, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("InventoryItem Location not implemented"))
 }
 
 func (r *purchaseResolver) Location(ctx context.Context, obj *model.Purchase) (*model.PurchaseLocation, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("Purchase Location not implemented"))
 }
 
 func (r *purchaseResolver) Items(ctx context.Context, obj *model.Purchase) ([]*model.PurchaseItem, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("Purchase Items not implemented"))
 }
 
 func (r *purchaseItemResolver) Item(ctx context.Context, obj *model.PurchaseItem) (*model.Item, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("PurchaseItem Item not implemented"))
 }
 
 func (r *purchaseItemResolver) Purchase(ctx context.Context, obj *model.PurchaseItem) (*model.Purchase, error) {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("PurchaseItem Purchase not implemented"))
 }
 
 func (r *ingredientInputResolver) Item(ctx context.Context, obj *model.IngredientInput, data *model.IngredientItemInput) error {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("IngredientInput Item not implemented"))
 }
 
 func (r *ingredientSetInputResolver) Ingredients(ctx context.Context, obj *model.IngredientSetInput, data []*model.IngredientInput) error {
-	panic(fmt.Errorf("not implemented"))
+	panic(fmt.Errorf("IngredientSetInput Ingredients not implemented"))
 }
 
 // Dish returns generated.DishResolver implementation.
